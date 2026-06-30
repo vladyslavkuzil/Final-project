b327eab30bcae415d49feda60993d47c97400ae3
Adam Poweska <adam.poweska@gmail.com>
2026-06-30 21:39:18 +0200

    added db_endpoint, db_port and db_name into terraform/outputs.tf file

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -37,3 +37,15 @@ output "db_subnet_group_name" {
   description = "Name of the RDS DB subnet group"
   value       = module.vpc.db_subnet_group_name
 }
+
+output "db_endpoint" {
+  value = module.database.db_endpoint
+}
+
+output "db_port" {
+  value = module.database.db_port
+}
+
+output "db_name" {
+  value = module.database.db_name
+}
