9b93578e15d3746707382424d55d7d078949a865
Adam Poweska <adam.poweska@gmail.com>
2026-07-01 20:17:58 +0200

    'scrum 28 implement project membership module #19'

---
+++ backend/alembic/versions/85ff86a72b0f_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 85ff86a72b0f
+Revises: 8c5d2a4d4bb2, e15ee09c44fb
+Create Date: 2026-07-01 19:58:26.882894
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '85ff86a72b0f'
+down_revision: Union[str, Sequence[str], None] = ('8c5d2a4d4bb2', 'e15ee09c44fb')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
