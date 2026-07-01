774f14acc34248ce8441e142ef5b2cc84900ff18
Bereznov Kyrylo <kbereznov@deloitte.com>
2026-07-01 16:09:48 +0200

    feat: add migration script for merging chat and join project modules

---
+++ backend/alembic/versions/e15ee09c44fb_merge_chat_and_join_codes_heads.py
@@ -0,0 +1,28 @@
+"""merge_chat_and_join_codes_heads
+
+Revision ID: e15ee09c44fb
+Revises: 02f1e1663b1d, 1dc572aa79f8
+Create Date: 2026-07-01 16:05:56.289311
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e15ee09c44fb'
+down_revision: Union[str, Sequence[str], None] = ('02f1e1663b1d', '1dc572aa79f8')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
