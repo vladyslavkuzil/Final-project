571800229178991ec42ce9a3516d3a32412c8830
Vladyslav Kuzil <vladyslavkuzil@gmail.com>
2026-07-06 14:11:49 +0300

    fix(frontend): fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
 };
 
 export default nextConfig;
